3ec4ac5e9d843bde809f7295c2fa046ec6e4e5be
Jerry Beau Baggett <117130414+jerrybeau89@users.noreply.github.com>
2023-02-02 12:47:56 -0800

    added uuid package, moved server.js, and made changes to index.js

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const notes = require('./notes');
+
+const app = express();
+
+app.use('./notes', notes);
+app.use('./api/notes', notes);
+
+module.exports = app;
