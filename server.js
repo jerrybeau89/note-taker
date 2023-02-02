f645f056c6ef022ca4add593a8d104ec1bbad94a
Jerry Beau Baggett <117130414+jerrybeau89@users.noreply.github.com>
2023-02-02 10:17:36 -0800

    set up start for server

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 const express = require('express');
+const path = require('path');
+const api = require('./Develop/routes/index');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//middleware for the app
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use('/api', api);
+app.use(express.static('public'));
+
+//get requests
+app.get('/', (req, res)=> 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res)=> 
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, ()=> 
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
