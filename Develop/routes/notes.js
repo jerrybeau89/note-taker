62012d27155b5e759d2c70ad230b193b4beb262e
Jerry Beau Baggett <117130414+jerrybeau89@users.noreply.github.com>
2023-02-02 12:04:49 -0800

    set get and post handlers for note.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,1 +1,46 @@
 const express = require('express');
+const notes = require('express').Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const util = require('util');
+const readFromFile = util.promisify(fs.readFile);
+const db = ('./db/db.json');
+
+notes.get('/', (req, res) => {
+  readFromFile(db).then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const {title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title, 
+      text,
+      note_id: uuidv4(),
+    };
+    fs.readFile(db, 'utf-8', (err, data)=>{
+      if (err) {
+        console.log(`There is a ${err} error!`);
+      } else {
+        const newData = JSON.parse(data);
+        newData.push(newNote);
+        res.json(newData);
+        fs.writeFile(db, JSON.stringify(newData, null, 4), (err, data)=> {
+          if (err) {
+            console.log(`There is a ${err} error!`);
+          } else {
+            console.log(`Note written to the ${db} file.`)
+          }
+        });
+      }
+    });
+  };
+});
+
+module.exports = notes;
+
+
--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.get('/notes', (req, res)=>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+//get request to return to index.html if there is an invalid request.
+app.get('*', (req, res)=> 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 app.listen(PORT, ()=> 
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
